497a7dd95153f49ff8d9c2e15d3b0f781d282f7e
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-10-24 11:36:44 +0200

    solved lightbulb turn on/off exerise

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 export const Lightbulb = () => {
   // Initialize the state for on/off
+  const [bulb, setBulb] = useState(false);
 
   const toggleLight = () => {
     // Toggle the on/off state
+    setBulb(!bulb)
+
     console.log("Clicked");
+   
   };
   return (
     <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
-        <div className="bulb"></div>
+      <div className= {bulb ? "lightbulb on" : "lightbulb off"} onClick={toggleLight}>
+        <div className="bulb">
+        </div>
       </div>
     </div>
   );
